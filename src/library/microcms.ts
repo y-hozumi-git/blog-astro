9892c1f54e87a03615016c144fc3d8bb7ca27224
y-hozumi-git <y.hozumi000@icloud.com>
2024-08-26 02:28:37 +0900

    ブログページのデザイン更新

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -1,7 +1,5 @@
 //SDK利用準備
-import type {
-  MicroCMSQueries
-} from "microcms-js-sdk";
+import type { MicroCMSQueries } from "microcms-js-sdk";
 import { createClient } from "microcms-js-sdk";
 
 const client = createClient({
